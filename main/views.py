0302ad0b37c5382ba1299e4324424e9a13b2fab7
ShahidulAbir <shahidulislam.net20@gmail.com>
2023-07-03 01:58:00 +0600

    Optimized table view

--- main/views.py
+++ main/views.py
@@ -9,26 +9,16 @@ def homepage(request):
     df = pd.read_excel("main/static/original.xlsx")
 
     df['Split'] = df['Time'].str.split()
-    df['Split'].astype('object')
 
-    df['Day'] = 0
-    df['Start_Time'] = 0
-    df['Finish_Time'] = 0
-    for index, row in df.iterrows():
-        if len(row['Split']) == 6:
-            df['Day'][index] = row['Split'][0]
-            df['Start_Time'][index] = f"{row['Split'][1]} {row['Split'][2]}"
-            df['Finish_Time'][index] = f"{row['Split'][4]} {row['Split'][5]}"
-        else:
-            df['Day'][index] = "TBA"
-            df['Start_Time'][index] = "TBA"
-            df['Finish_Time'][index] = "TBA"
+    df['Day'] = df['Split'].apply(lambda x: x[0] if len(x) == 6 else "TBA")
+    df['Start_Time'] = df['Split'].apply(lambda x: f"{x[1]} {x[2]}" if len(x) == 6 else "TBA")
+    df['Finish_Time'] = df['Split'].apply(lambda x: f"{x[4]} {x[5]}" if len(x) == 6 else "TBA")
 
     df = df.drop(['Time', 'Split', 'Unnamed: 0'], axis=1)
+
     df.rename(columns={'Seats Available': 'Seats_Available'}, inplace=True)
     df.index += 1
 
     json_records = df.reset_index().to_json(orient='records')
     data = json.loads(json_records)
-    print(data[:10])
     return render(request, 'main/homepage.html', {"data": data})
