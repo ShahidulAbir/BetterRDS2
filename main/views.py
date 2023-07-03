d5ec0675a7f27483d26ee8f9eb311d1ba280c8b7
ShahidulAbir <shahidulislam.net20@gmail.com>
2023-07-03 18:48:24 +0600

    Sorted Table View

--- main/views.py
+++ main/views.py
@@ -9,16 +9,20 @@ def homepage(request):
     df = pd.read_excel("main/static/original.xlsx")
 
     df['Split'] = df['Time'].str.split()
+    df = df.drop(['Time', 'Unnamed: 0'], axis=1)
 
     df['Day'] = df['Split'].apply(lambda x: x[0] if len(x) == 6 else "TBA")
     df['Start_Time'] = df['Split'].apply(lambda x: f"{x[1]} {x[2]}" if len(x) == 6 else "TBA")
     df['Finish_Time'] = df['Split'].apply(lambda x: f"{x[4]} {x[5]}" if len(x) == 6 else "TBA")
 
-    df = df.drop(['Time', 'Split', 'Unnamed: 0'], axis=1)
+    df = df.drop(['Split'], axis=1)
+
+    df['Formatted_Time'] = pd.to_datetime(df['Start_Time'], format='%I:%M %p', errors='coerce')
+    df = df.sort_values(['Course', 'Day', 'Formatted_Time', 'Section'])
 
     df.rename(columns={'Seats Available': 'Seats_Available'}, inplace=True)
-    df.index += 1
 
     json_records = df.reset_index().to_json(orient='records')
     data = json.loads(json_records)
+
     return render(request, 'main/homepage.html', {"data": data})
